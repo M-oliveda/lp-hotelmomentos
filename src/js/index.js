8d3fe3491c210a72ef5244f976da9cb80f1960fa
Mauricio Oliveda <moliveda@protonmail.com>
2022-09-12 15:06:44 -0300

    Making changes to improve the quality of site. Plus, reparing some fixes.

--- src/js/index.js
+++ src/js/index.js
@@ -8,16 +8,43 @@ document.getElementById("exitSidebar").addEventListener("click", (e) => {
 });
 
 // Control the behaviour of the contactForm
+document.getElementById("contactForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  location.replace("./confirmation.html");
+});
+
 document.querySelectorAll("input, textarea").forEach((element) => {
   element.addEventListener("invalid", (e) => {
-    if (e.target.value === "")
-      e.target.setCustomValidity("Debe ingresar un valor correcto.");
-    else e.target.setCustomValidity("");
+    switch (e.target.name) {
+      case "fullname":
+        e.target.setCustomValidity(
+          "Debe ingresar un nombre válido. Recuerde que el campo debe estar compuesto por su nombre y apellido, separados por un espacio en blanco."
+        );
+        e.target.classList.add(`text-input_invalid`);
+        break;
+      case "email":
+        e.target.setCustomValidity(
+          "Debe ingresar un email válido. Para su orientación, este es un correo electrónico válido: ejemplo@ejemplo.com"
+        );
+        e.target.classList.add(`text-input_invalid`);
+        break;
+      case "message":
+        e.target.setCustomValidity(
+          "Por favor, ingrese un mensaje con toda la información que considere importante."
+        );
+        e.target.classList.add(`text-input_invalid`);
+        break;
+    }
   });
 
-  element.addEventListener("input", (e) => {
+  element.addEventListener("focusout", (e) => {
     e.target.setCustomValidity("");
-    e.target.checkValidity();
+    let result = e.target.checkValidity();
+
+    if (result) {
+      e.target.classList.remove("text-input_invalid");
+      e.target.classList.add("text-input_valid");
+    }
   });
 });
 
@@ -25,13 +52,11 @@ document.querySelectorAll("input[type=checkbox]").forEach((element) => {
   element.addEventListener("invalid", (e) => {
     if (!e.target.checked) {
       e.target.setCustomValidity("Debe aceptar las condiciones de servicio");
+      e.target.classList.add("input-text_invalid");
     } else {
       e.target.setCustomValidity("");
+      e.target.classList.remove("input-text_invalid");
+      e.target.classList.add("input-text_valid");
     }
   });
-});
-
-document.getElementById("contactForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-  location.replace("./confirmation.html");
 });
